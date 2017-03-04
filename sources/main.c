17f1f64a6bb250de0368044a15ba3e3a0a25a2d7
Mathis Pandelé <mapandel@student.42.fr>
2017-03-04 14:44:57 +0100

    main.c 1.2 : More init + del p4

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 22:46:38 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/04 13:09:38 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/04 14:44:17 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void		del_p4(t_p4 *p4)
 	int		tmp;
 
 	tmp = 0;
-	while (tmp < 7)
+	while (tmp <= p4->lines)
 	{
 		ft_strdel(&p4->grid[tmp]);
 		++tmp;
@@ -26,20 +26,26 @@ static void		del_p4(t_p4 *p4)
 	ft_memdel((void**)p4);
 }
 
-static t_p4		*init_p4(t_p4 *p4)
+static t_p4		*init_p4(t_p4 *p4, char **argv)
 {
 	int		tmp;
 
 	tmp = 0;
-	p4 = ft_memalloc(sizeof(t_p4));
-	p4->grid = ft_memalloc(sizeof(char**) * 7);
-	p4->grid[7] = NULL;
-	while (tmp < 7)
+	if (!(p4 = ft_memalloc(sizeof(t_p4))) || !(p4->grid =
+			ft_memalloc(sizeof(char**) * (unsigned long)(ft_atoi(argv[1]) + 1))))
+		return (NULL);
+	p4->lines = ft_atoi(argv[1]);
+	p4->columns = ft_atoi(argv[2]);
+	p4->grid[p4->lines + 1] = NULL;
+	while (tmp < p4->lines)
 	{
-		p4->grid[tmp] = ft_strnew(6);
-		p4->grid[tmp] = ft_strfill(p4->grid[tmp], 0, 6);
+		if (!(p4->grid[tmp] = ft_strnew((size_t)p4->columns)))
+			return (NULL);
+		p4->grid[tmp] = ft_strfill(p4->grid[tmp], ' ', (size_t)p4->columns);
 		++tmp;
 	}
+	p4->x = 0;
+	p4->y = 0;
 	return (p4);
 }
 
@@ -48,10 +54,22 @@ int				main(int argc, char **argv)
 	t_p4	*p4;
 
 	p4 = NULL;
-	p4 = init_p4(p4);
+	(void)argc;
+	if (!argv[1] || !argv[2] || !argv[3] || !argv[4]
+		|| ft_atoi(argv[1]) < 6 || ft_atoi(argv[1]) > 99
+		|| ft_atoi(argv[2]) < 7 || ft_atoi(argv[2]) > 99
+		|| !(ft_atoi(argv[3]) == 1 || ft_atoi(argv[3]) == 2)
+		|| !(ft_atoi(argv[4]) == 1 || ft_atoi(argv[4]) == 2))
+	{
+		ft_putstr("usage: puissance4 [5 < lines < 100] [6 < columns < 100] ");
+		ft_putstr("[J1: Humain - 1 / AI - 2] [J2: Humain - 1 / AI - 2] ");
+		ft_putendl("[J3+...]");
+		return (-1);
+	}
+	p4 = init_p4(p4, argv);
+	p4->lines = ft_atoi(argv[1]);
+	p4->columns = ft_atoi(argv[2]);
 	display_grid(p4);
 	del_p4(p4);
-	(void)argc;
-	(void)argv;
 	return (0);
 }
