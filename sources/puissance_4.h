c633002df2912d51b80dad3ffc6946adccff359c
Mathis Pandelé <mapandel@student.42.fr>
2017-03-04 13:12:02 +0100

    puissance_4.h 1.1 : Struct p4

--- sources/puissance_4.h
+++ sources/puissance_4.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 22:47:23 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/03 22:49:13 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/04 12:54:23 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 # define PUISSANCE_4_H
 
 #include "../libft/libft.h"
+
+typedef struct	s_p4
+{
+	char		**grid;
+	int			x;
+	int			y;
+}				t_p4;
+
+void	display_grid(t_p4 *p4);
 
 #endif
