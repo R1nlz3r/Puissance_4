bfec942b9954597abe1e63e03a38a941b4cd8960
Mathis Pandelé <mapandel@student.42.fr>
2017-03-04 16:43:34 +0100

    puissance_4.h 1.3 : Struct p4 + Prototypes

--- sources/puissance_4.h
+++ sources/puissance_4.h
@@ -6,14 +6,14 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 22:47:23 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/04 14:45:33 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/04 16:40:48 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUISSANCE_4_H
 # define PUISSANCE_4_H
 
-#include "../libft/libft.h"
+# include "../libft/libft.h"
 
 typedef struct	s_p4
 {
@@ -22,8 +22,14 @@ typedef struct	s_p4
 	int			y;
 	int			columns;
 	int			lines;
+	int			j1;
+	int			j2;
+	int			scorej1;
+	int			scorej2
+	int			pad_0;
 }				t_p4;
 
-void	display_grid(t_p4 *p4);
+void		game(t_p4 *p4);
+void		display_grid(t_p4 *p4);
 
 #endif
