14293f4ee67406862650f688b840aeebeda314c1
Mathis Pandelé <mapandel@student.42.fr>
2017-03-05 10:54:33 +0100

    game.c 1.3 : Rerun

--- sources/game.c
+++ sources/game.c
@@ -6,11 +6,35 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/04 15:55:52 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/05 07:06:39 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/05 10:49:07 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "puissance_4.h"
+
+static int		rerun(t_p4 *p4)
+{
+	char	*buf;
+
+	buf = ft_strnew(1000000);
+	ft_putendl("\n> Rejouer? 1: Oui. 2: Non.");
+	if (read(0, buf, 1000000) == -1)
+		return (-1);
+	while (!(ft_atoi(buf) == 1 || ft_atoi(buf) == 2))
+	{
+		ft_putendl("> Input invalide, réesaye.");
+		ft_strclr(buf);
+		if (read(0, buf, 1000000) == -1 || buf[999999])
+			return (-1);
+	}
+	if (ft_atoi(buf) == 1)
+		p4->rerun = 1;
+	else
+		p4->rerun = 0;
+	clear_grid(p4);
+	ft_strdel(&buf);
+	return (0);
+}
 
 static int		j_plays(t_p4 *p4)
 {
@@ -18,14 +42,14 @@ static int		j_plays(t_p4 *p4)
 
 	buf = ft_strnew(1000000);
 	if (p4->res == J1_PLAYING)
-		ft_putendl("\n> Joueur 1 : À toi de jouer");
+		ft_putendl("\n> \033[31mJoueur 1\033[0m : À toi de jouer");
 	else if (p4->res == J2_PLAYING)
-		ft_putendl("\n> Joueur 2 : À toi de jouer");
+		ft_putendl("\n> \033[33mJoueur 2\033[0m : À toi de jouer");
 	if (read(0, buf, 1000000) == -1)
 		return (-1);
 	while (check_valid_input(p4, ft_atoi(buf) - 1) == -1)
 	{
-		ft_putendl("Input invalide, réesaye.");
+		ft_putendl("> Input invalide, réesaye.");
 		ft_strclr(buf);
 		if (read(0, buf, 1000000) == -1 || buf[999999])
 			return (-1);
@@ -61,17 +85,17 @@ int		game(t_p4 *p4)
 			display_grid(p4);
 			if (j_plays(p4) == -1)
 				return (-1);
-			display_grid(p4);
 			check_end_game(p4);
 			if (p4->res == J1_PLAYING)
 				p4->res = J2_PLAYING;
 			else if (p4->res == J2_PLAYING)
 				p4->res = J1_PLAYING;
 		}
+		display_grid(p4);
 		display_score(p4);
+		if (rerun(p4) == -1)
+			return (-1);
 		//hightlight le résultat
-		//demande de rerun (si oui -> reset)
-		p4->rerun = 0;
 	}
 	return (0);
 }
